890ee69bd00c01e8b4588f120ea41f45c5ece4a9
Misha Holzman <mcholzman1@gmail.com>
2018-09-25 22:42:02 -0400

    Created schema with sequelize and created fooder_dev database with appropriate tables and associations

--- models/index.js
+++ models/index.js
@@ -4,50 +4,91 @@ const Sequelize = require('sequelize');
 // connect to the database and set up some behavior
 const db = new Sequelize({
   database: 'fooder_dev',
-  dialect:  'postgres',
-  define:   {
+  dialect: 'postgres',
+  define: {
     underscored: true,
-    returning:   true,
+    returning: true,
   },
 });
 
-// Recipe has many ingredients and has many categories
+
 const Recipe = db.define('recipe', {
-  name:    Sequelize.STRING(64),
-  img_url: Sequelize.TEXT,
-  image: ,
-  cook_time: ,
-  calories_per_serving: ,
-  instructions: ,
+  name: {
+    type:      Sequelize.STRING(64),
+    unique:    true,
+    allowNull: false,
+  },
+  img_url: {
+    type:      Sequelize.TEXT,
+    allowNull: false,
+  },
+  yeild: {
+    type:      Sequelize.INTEGER,
+    allowNull: false,
+  },
+  total_cook_time: {
+    type:      Sequelize.INTEGER,
+    allowNull: false,
+  },
+  calories_per_serving: {
+    type:      Sequelize.INTEGER,
+  },
+  description: {
+    type:      Sequelize.TEXT,
+  },
+  directions: {
+    type:      Sequelize.TEXT,
+    allowNull: false,
+  }
 });
 
-// Ingredient has many recipes
+
 const Ingredient = db.define('ingredient', {
-  name: Sequelize.STRING(32),
-
+  name: {
+    type:      Sequelize.STRING(64),
+    allowNull: false,
+  }
 });
 
 
-// Categories has many Recipes
 const Category = db.define('category', {
-  cuisine_type: Sequelize.STRING(32),
-  name: Sequelize.STRING(32),
-  image: Sequelize.STRING(255),
+  cuisine_type: {
+    type:      Sequelize.STRING(32),
+  },
+  cook_time: {
+    type:      Sequelize.INTEGER,
+    allowNull: false,
+  },
+  max_calories: {
+    type:      Sequelize.INTEGER,
+  },
+  vegitarian: {
+    type:      Sequelize.BOOLEAN,
+  },
+  meat: {
+    type:      Sequelize.BOOLEAN,
+  }
+});
+
+
+const RecipeIngredient = db.define('recipe_ingredients', {
+  quantity: Sequelize.STRING(64),
 });
 
 
 // ASSOCIATIONS
 
-// House.hasMany(Student);
-// Student.belongsTo(House);
+Recipe.belongsToMany(Ingredient, { through: 'recipe_ingredients' });
+Ingredient.belongsToMany(Recipe, { through: 'recipe_ingredients' });
 
-// House.belongsToMany(Color, { through: 'house_color_xref' });
-// Color.belongsToMany(House, { through: 'house_color_xref' });
+Recipe.belongsToMany(Category, { through: 'recipe_category_xref' });
+Category.belongsToMany(Recipe, { through: 'recipe_category_xref' });
 
 
 module.exports = {
   Recipe,
   Ingredient,
   Category,
+  RecipeIngredient,
   db,
 };
